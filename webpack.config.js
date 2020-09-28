bf152e17aefc5b0db777ed0f3a8e0577945aa327
FerCafferata <fercafferata77@gmail.com>
2020-09-28 19:55:25 -0300

    modif

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename:'main.js',
     },
     resolve: {
-        extensions:['.js']
-    },
+        extensions: ['.js'],
+
+},
     module: {
-        rules:[
+        rules: [
             {
-                test: /\.js?$/,
+                test:/\.js?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: './public/index.html',
-            filname: './index.html'
-        }),
+        new HtmlWebpackPlugin(
+            {
+                inject: true,
+                template:'./public/index.html',
+                filename:'./index.html'
+            }
+        ),
         new CopyWebpackPlugin({
             patterns: [
               { from: './src/styles/styles.css', to: './' },
             ],
           }),
-    
+
+        new MiniCssExtractPlugin()
     ]
 }
+
